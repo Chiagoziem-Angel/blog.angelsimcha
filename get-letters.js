872ed1adcef1db0ae1024b796967deeeb5080de1
Chiagoziem Angel <114437175+Chiagoziem-Angel@users.noreply.github.com>
2026-04-15 14:36:02 +0100

    Update get-letters.js

--- get-letters.js
+++ get-letters.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://api.convertkit.com/v3/broadcasts?api_secret=${apiSecret}`
+      `https://angelsimcha.kit.com/v3/broadcasts?api_secret=${apiSecret}`
     );
 
     if (!response.ok) {
